015102ee2d7f82c320865812a66520168b2df4ea
Viv Richards <vivrichards600@hotmail.com>
2017-10-18 19:59:06 +0100

    chaging travis yml to utf-8

--- AutomatedVisualTesting/ExampleTests.cs
+++ AutomatedVisualTesting/ExampleTests.cs
@@ -1,11 +1,28 @@
 using AutomatedVisualTesting.Utilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace AutomatedVisualTesting
 {
     [TestClass]
     public class ExampleTests
     {
+
+        private static IWebDriver _driver;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _driver.Quit();
+        }
+
         [TestMethod]
         public void NoDifferenceBetweenImageAndScreenshotFromUrl()
         {
@@ -35,6 +52,31 @@ namespace AutomatedVisualTesting
             // Assert
             Assert.IsTrue(difference == 0);
         }
+
+
+
+
+
+        [TestMethod]
+        public void CodeTest()
+        {
+            // Arrange
+            IWebDriver _driver = new ChromeDriver();        
+
+            string baseImage = "Table.png";
+            string url = "http://computer-database.herokuapp.com/computers";
+            string elementSelector = ".computers";
+            // SeleniumDriver.CoverDynamicElementBySelector(".computers");
+
+            // Act
+            int difference = Compare.GetDifference(baseImage, url, elementSelector);
+
+            // Assert
+            Assert.IsTrue(difference == 0);
+        }
+
+
+
 
     }
 }
