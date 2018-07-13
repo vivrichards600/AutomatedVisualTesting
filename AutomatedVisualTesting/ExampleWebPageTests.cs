544bfefc8cfcedcbfc89e4f423e2d373adb4b8a3
viv <viv.richards@vizolution.co.uk>
2018-07-13 10:31:32 +0100

    examples updated

--- AutomatedVisualTesting/ExampleWebPageTests.cs
+++ AutomatedVisualTesting/ExampleWebPageTests.cs
@@ -13,6 +13,54 @@ namespace AutomatedVisualTesting
         public static IWebDriver Driver;
         private readonly ComparisonOptions _options = new ComparisonOptions();
 
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Driver.Quit();
+        }
+
+        [TestMethod]
+        public void Full_Page_Comparison_Covering_Dynamic_Element_Test()
+        {
+            _options.ShowCellValues = true;
+
+            Driver.Navigate().GoToUrl("https://computer-database.herokuapp.com/computers");
+            var baseImage = "ComputerDatabaseHomePageWithoutHeadingAndTable.Chrome.png";
+
+            CoverDynamicElementBySelector(Driver, ".computers");
+
+            _options.Threshold = 20;
+
+            var result = Differences(baseImage, Driver, _options);
+
+            Assert.IsTrue(result.Match);
+        }
+
+        [TestMethod]
+        public void Full_Page_Comparison_Test()
+        {
+            // Optionally setting show cell values and passing _options gives you a difference image with tolerance levels drawn!
+            _options.ShowCellValues = true;
+
+            Driver.Navigate().GoToUrl("https://computer-database.herokuapp.com/computers/new");
+            var baseImage = "ComputerDatabaseAddComputerPage.Chrome.png";
+            var result = Differences(baseImage, Driver, _options);
+
+            Assert.IsTrue(result.Match);
+        }
+
+        [TestMethod]
+        public void Individual_Page_Element_Comparison_Test()
+        {
+            Driver.Navigate().GoToUrl("https://computer-database.herokuapp.com/computers");
+            var element = "add";
+            var baseImage = "ComputerDatabaseAddButton.Chrome.png";
+
+            var result = Differences(baseImage, element, Driver);
+
+            Assert.IsTrue(result.Match);
+        }
+
         [TestInitialize]
         public void Startup()
         {
@@ -22,47 +70,6 @@ namespace AutomatedVisualTesting
             chromeOptions.AddArguments("disable-infobars");
 
             Driver = new ChromeDriver(chromeOptions);
-        }
-
-        [TestCleanup]
-        public void Cleanup()
-        {
-            Driver.Quit();
-        }
-
-        [TestMethod]
-        public void Full_Page_Comparison_Test()
-        {
-            Driver.Navigate().GoToUrl("https://www.warnerbros.com/archive/spacejam/movie/jam.html");
-            var baseImage = "SpaceJamHomepage.Chrome.png";
-            var result = Differences(baseImage, Driver, _options);
-
-            Assert.IsTrue(result.Match);
-        }
-
-        [TestMethod]
-        public void Full_Page_Comparison_Covering_Dynamic_Element_Test()
-        {
-            Driver.Navigate().GoToUrl("https://www.warnerbros.com/archive/spacejam/movie/jam.html");
-            var baseImage = "SpaceJamHomepageWithoutFooterLink.Chrome.png";
-
-            CoverDynamicElementBySelector(Driver, ".footer-links");
-
-            var result = Differences(baseImage, Driver, _options);
-
-            Assert.IsTrue(result.Match);
-        }
-
-        [TestMethod]
-        public void Individual_Page_Element_Comparison_Test()
-        {
-            Driver.Navigate().GoToUrl("https://www.warnerbros.com/archive/spacejam/movie/jam.html");
-            var element = "img";
-            var baseImage = "SpaceJamShuttleImage.Chrome.png";
-
-            var result = Differences(baseImage, element, Driver);
-
-            Assert.IsTrue(result.Match);
         }
     }
 }
